5757ecd9b884d38e98ff9c31b7657202e703f7be
Vinybsqn <vianneybasquin59@gmail.com>
2024-04-04 14:11:13 +0200

    Update privilegesSeed.ts

--- prisma/seed/privilegesSeed.ts
+++ prisma/seed/privilegesSeed.ts
@@ -3,8 +3,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const roles = ['coach', 'contributeur', 'joueur']; // different roles du brief
-
+  const roles = ['Coach', 'Contributeur', 'Joueur'];
 
   for (const name of roles) {
     await prisma.privileges.create({
@@ -15,28 +14,6 @@ async function main() {
   }
 
   console.log(`Roles [${roles.join(', ')}] created.`);
-
-  const CoachPrivilege = {
-    data: {
-      name: 'Coach',
-    },
-  };
-
-  const ContributeurPrivilege = {
-    data: {
-      name: 'Contributeur',
-    },
-  };
-
-  const JoueurPrivilege = {
-    data: {
-      name: 'Joueur',
-    },
-  };
-
-  await prisma.privileges.create(CoachPrivilege);
-  await prisma.privileges.create(ContributeurPrivilege);
-  await prisma.privileges.create(JoueurPrivilege);
 }
 
 main()
