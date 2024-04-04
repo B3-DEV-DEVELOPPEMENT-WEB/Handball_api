56cfc2cf00cf7b1253abbf0cc6f7ff39732b8834
Thibaut009 <gabet.thibaut@gmail.com>
2024-04-04 14:16:08 +0200

    update

--- prisma/seed/privilegesSeed.ts
+++ prisma/seed/privilegesSeed.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-
   const roles = ['Coach', 'Contributeur', 'Joueur'];
 
   for (const name of roles) {
